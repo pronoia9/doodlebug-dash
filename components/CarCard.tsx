dd655581c810de8a862696cd7b162c8a4be98ccf
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-26 22:35:54 +0300

    delete classnames from icons to fix sizes

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -33,15 +33,15 @@ const CarCard = ({ car }: { car: CarProps }) => {
       <div className='relative flex w-full mt-2'>
         <div className='flex group-hover:invisible w-full justify-between text-grey'>
           <div className='flex flex-col justify-center items-center gap-2'>
-            <Image className='w-auto h-auto' src='/steering-wheel.svg' width={20} height={20} alt='steering wheel' />
+            <Image src='/steering-wheel.svg' width={20} height={20} alt='steering wheel' />
             <p className='text-[14px] leading-[17px]'>{transmission === 'a' ? 'Automatic' : 'Manual'}</p>
           </div>
           <div className='car-card__icon'>
-            <Image className='w-auto h-auto' src='/tire.svg' width={20} height={20} alt='seat' />
+            <Image src='/tire.svg' width={20} height={20} alt='seat' />
             <p className='car-card__icon-text'>{drive.toUpperCase()}</p>
           </div>
           <div className='car-card__icon'>
-            <Image className='w-auto h-auto' src='/gas.svg' width={20} height={20} alt='gas' />
+            <Image src='/gas.svg' width={20} height={20} alt='gas' />
             <p className='car-card__icon-text'>{city_mpg} MPG</p>
           </div>
         </div>
