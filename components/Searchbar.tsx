d6c409cc1e0dd4e58462a051637e264c337f90a7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-26 22:05:50 +0300

    add useeffect to filter cars on any input change

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 
@@ -11,7 +11,30 @@ const Searchbar = () => {
   const [model, setModel] = useState('');
   const router = useRouter();
 
-  const handleSearch = () => {};
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (manufacturer.trim() === '' && model.trim() === '') return alert('Please provide some input');
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+  };
+
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    // Create a new URLSearchParams object using the current URL search parameters
+    const searchParams = new URLSearchParams(window.location.search);
+    // Update or delete the 'model' search parameter based on the 'model' value
+    if (model) searchParams.set('model', model);
+    else searchParams.delete('model');
+    // Update or delete the 'manufacturer' search parameter based on the 'manufacturer' value
+    if (manufacturer) searchParams.set('manufacturer', manufacturer);
+    else searchParams.delete('manufacturer');
+    // Generate the new pathname with the updated search parameters
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+    router.push(newPathname, { scroll: false });
+  };
+
+  useEffect(() => {
+    const search = setTimeout(() => { updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase()); }, 500);
+    return () => { clearTimeout(search); };
+  }, [manufacturer, model]);
 
   return (
     <form className='searchbar' onSubmit={handleSearch}>
@@ -19,7 +42,6 @@ const Searchbar = () => {
         <SearchManufacturer manufacturer={manufacturer} setManuFacturer={setManuFacturer} />
         <SearchButton otherClasses='sm:hidden' />
       </div>
-
       <div className='searchbar__item'>
         <Image src='/model-icon.png' width={25} height={25} className='absolute w-[20px] h-[20px] ml-4' alt='car model' />
         <input
