9e2aef9b5c358ce2124ec5bbf29b9a186bc25c84
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-26 17:53:43 +0300

    add init custom button

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,4 +1,12 @@
-const CustomButton = () => {
-  return <div>CustomButton</div>;
+'use client';
+import Image from 'next/image';
+import { CustomButtonProps } from '@types';
+
+const CustomButton = ({ isDisabled, btnType, containerStyles, textStyles, title, rightIcon, handleClick }: CustomButtonProps) => {
+  return (
+    <button className={`custom-btn ${containerStyles}`} disabled={isDisabled} type={btnType || 'button'} onClick={handleClick}>
+      CustomButton
+    </button>
+  );
 };
 export default CustomButton;
