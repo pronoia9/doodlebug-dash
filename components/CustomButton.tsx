2439c51efe349704a54c6799f87c87c8ab665422
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-26 17:54:56 +0300

    format + add icons to button

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,12 +1,18 @@
 'use client';
+
 import Image from 'next/image';
+
 import { CustomButtonProps } from '@types';
 
-const CustomButton = ({ isDisabled, btnType, containerStyles, textStyles, title, rightIcon, handleClick }: CustomButtonProps) => {
-  return (
-    <button className={`custom-btn ${containerStyles}`} disabled={isDisabled} type={btnType || 'button'} onClick={handleClick}>
-      CustomButton
-    </button>
-  );
-};
-export default CustomButton;
+const Button = ({ isDisabled, btnType, containerStyles, textStyles, title, rightIcon, handleClick }: CustomButtonProps) => (
+  <button className={`custom-btn ${containerStyles}`} type={btnType || 'button'} disabled={isDisabled} onClick={handleClick}>
+    <span className={`flex-1 ${textStyles}`}>{title}</span>
+    {rightIcon && (
+      <div className='relative w-6 h-6'>
+        <Image className='object-contain' src={rightIcon} alt='arrow_left' fill />
+      </div>
+    )}
+  </button>
+);
+
+export default Button;
