2ab985004a810771b902ce033356c3cb7a87f0c7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-26 20:19:38 +0300

    fetch + map cars

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,16 @@ import { fuels, yearsOfProduction } from '@constants';
 import { HomeProps } from '@types';
 import { fetchCars } from '@utils';
 
-export default function Home() {
+export default async function Home({ searchParams }: HomeProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || '',
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || '',
+    limit: searchParams.limit || 10,
+    model: searchParams.model || '',
+  });
+  const isDataEmpty = !allCars || !Array.isArray(allCars) || !allCars?.length;
+  
   return (
     <main className='overflow-hidden'>
       <Hero />
@@ -22,6 +31,22 @@ export default function Home() {
             <CustomFilter title='year' options={yearsOfProduction} />
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+            <ShowMore pageNumber={(searchParams.limit || 10) / 10} isNext={(searchParams.limit || 10) > allCars.length} />
+          </section>
+        ) : (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops, no results.</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
