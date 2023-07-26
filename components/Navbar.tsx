415621413be23bdcdf83e46536a9c917159fae5d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-26 18:16:25 +0300

    finish simpleton navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,18 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import CustomButton from './CustomButton';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className='w-full absolute z-10'>
+      <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent'>
+        <Link href='/' className='flex justify-center items-center'>
+          <Image className='object-contain' src='/logo.svg' alt='logo' width={118} height={18} />
+        </Link>
+        <CustomButton containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]' title='Sign In' btnType='button' />
+      </nav>
+    </header>
+  );
 };
 export default Navbar;
