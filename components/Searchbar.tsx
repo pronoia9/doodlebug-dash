2762c1582da36a5f134568433f0e863852adb19b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-26 19:56:22 +0300

    add init searchbar contents

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,4 +1,39 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+
+import { SearchButton, SearchManufacturer } from '.';
+
 const Searchbar = () => {
-  return <div>Searchbar</div>;
+  const [manufacturer, setManuFacturer] = useState('');
+  const [model, setModel] = useState('');
+  const router = useRouter();
+
+  const handleSearch = () => {};
+
+  return (
+    <form className='searchbar' onSubmit={handleSearch}>
+      <div className='searchbar__item'>
+        <SearchManufacturer manufacturer={manufacturer} setManuFacturer={setManuFacturer} />
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
+
+      <div className='searchbar__item'>
+        <Image src='/model-icon.png' width={25} height={25} className='absolute w-[20px] h-[20px] ml-4' alt='car model' />
+        <input
+          type='text'
+          name='model'
+          value={model}
+          onChange={(e) => { setModel(e.target.value); }}
+          placeholder='Tiguan...'
+          className='searchbar__input'
+        />
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
+      <SearchButton otherClasses='max-sm:hidden' />
+    </form>
+  );
 };
 export default Searchbar;
