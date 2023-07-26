2d96f387c5746eed2b52e586caf6140cfbaed02f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-26 17:53:10 +0300

    add init hero content

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,27 @@
+'use client';
+
 import Image from 'next/image';
 
+import { CustomButton } from '@components';
+
 const Hero = () => {
-  return <div>Hero</div>;
+  const handleScroll = () => { }
+  
+  return (
+    <div className='hero'>
+      <div className='flex-1 pt-36 padding-x'>
+        <h1 className='hero__title'>Find, book, rent a car—quick and super easy!</h1>
+        <p className='hero__subtitle'>Streamline your car rental experience with our effortless booking process.</p>
+        <CustomButton containerStyles='bg-primary-blue text-white rounded-full mt-10' title='Explore Cars' handleClick={handleScroll} />
+      </div>
+
+      <div className='hero__image-container'>
+        <div className='hero__image'>
+          <Image className='object-contain' src='/hero.png' alt='hero' fill />
+        </div>
+        <div className='hero__image-overlay' />
+      </div>
+    </div>
+  );
 };
 export default Hero;
