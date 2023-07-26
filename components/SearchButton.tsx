51c6c21a75ddd393b5f8a88a4161bee6811e7ec8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-26 20:46:18 +0300

    finish init search button

--- components/SearchButton.tsx
+++ components/SearchButton.tsx
@@ -1,4 +1,10 @@
-const SearchButton = () => {
-  return <div>SearchButton</div>;
+import Image from 'next/image';
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => {
+  return (
+    <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
+      <Image src={'/magnifying-glass.svg'} alt={'magnifying glass'} width={40} height={40} className='object-contain' />
+    </button>
+  );
 };
 export default SearchButton;
