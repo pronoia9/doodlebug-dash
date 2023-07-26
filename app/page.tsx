7a2ebe3d61a4e4db829c1fa8219ccff12f3c87d6
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-26 19:55:31 +0300

    add filters container

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
-import Image from 'next/image';
-import { Hero } from '@/components';
+import { CarCard, ShowMore, Searchbar, CustomFilter, Hero } from '@components';
+import { fuels, yearsOfProduction } from '@constants';
+import { HomeProps } from '@types';
+import { fetchCars } from '@utils';
 
 export default function Home() {
   return (
     <main className='overflow-hidden'>
       <Hero />
+
+      <div id='discover' className='mt-12 padding-x padding-y max-width'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
+        </div>
+
+        <div className='home__filters'>
+          <Searchbar />
+
+          <div className='home__filter-container'>
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
